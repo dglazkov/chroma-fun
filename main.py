c877cde76caf3839639563017594d22199d19e5d
Dimitri Glazkov <dimitri@glazkov.com>
2023-04-27 19:52:03 -0700

    Get the basics going.

---
+++ main.py
@@ -0,0 +1,21 @@
+import chromadb
+
+client = chromadb.Client()
+
+collection = client.create_collection("all-my-documents")
+
+collection.add(
+    documents=["This is document1", "This is document2"],
+    metadatas=[{"source": "notion"}, {"source": "google-docs"}],
+    ids=["doc1", "doc2"],
+)
+
+# Query/search 2 most similar results. You can also .get by id
+results = collection.query(
+    query_texts=["This is a query document"],
+    n_results=2,
+    # where={"metadata_field": "is_equal_to_this"}, # optional filter
+    # where_document={"$contains":"search_string"}  # optional filter
+)
+
+print(results)
